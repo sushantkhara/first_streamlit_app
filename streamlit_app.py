0946b5be9729695021811daa6232d8a85e8f6573
Sushant Khara <sushantkumarkhara@gmail.com>
2023-11-03 07:58:05 +0530

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,6 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
 streamlit.text('🥗 Kale , Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.test('🥑🍞 Avocado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
