ad1cde1ade6c718c3883bf756c17c0a6b4e51abd
Sushant Khara <sushantkumarkhara@gmail.com>
2023-11-03 07:54:50 +0530

    Update streamlit_app.py
    emojis added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 streamlit.title('My parent\'s healthy Diner!')
 
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Oatmeal')
-streamlit.text('Kale , Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣 Omega 3 and Blueberry Oatmeal')
+streamlit.text('🥗 Kale , Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.test('🥑🍞 Avocado Toast')
