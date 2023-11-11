5418778f7a12480b8eb5a1ba2fccb415b577e055
Sushant Khara <sushantkumarkhara@gmail.com>
2023-11-11 12:45:12 +0530

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -4,43 +4,56 @@ import pandas as pd
 
 streamlit.title("Zena's Amazing Athleisure Catalog")
 
-# Connect to Snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+try:
+    # Check if running in Streamlit environment
+    streamlit.secrets
 
-# Run a Snowflake query and put it all in a variable called my_catalog
-my_cur.execute("SELECT color_or_style FROM catalog_for_website")
-my_catalog = my_cur.fetchall()
+    # Connect to Snowflake
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_cur = my_cnx.cursor()
 
-# Put the data into a DataFrame
-df = pd.DataFrame(my_catalog)
+    # Run a Snowflake query and put it all in a variable called my_catalog
+    my_cur.execute("SELECT color_or_style FROM catalog_for_website")
+    my_catalog = my_cur.fetchall()
 
-# Temporarily write the DataFrame to the page so I can see what I am working with
-# streamlit.write(df)
+    # Put the data into a DataFrame
+    df = pd.DataFrame(my_catalog)
 
-# Put the first column into a list
-color_list = df[0].values.tolist()
+    # Temporarily write the DataFrame to the page so I can see what I am working with
+    # streamlit.write(df)
 
-# Let's put a pick list here so they can pick the color
-option = streamlit.selectbox('Pick a sweatsuit color or style:', color_list)
+    # Put the first column into a list
+    color_list = df[0].values.tolist()
 
-# We'll build the image caption now since we can
-product_caption = f'Our warm, comfortable, {option} sweatsuit!'
+    # Let's put a pick list here so they can pick the color
+    option = streamlit.selectbox('Pick a sweatsuit color or style:', color_list)
 
-# Use the selected option to retrieve all the info from the database using a parameterized query
-my_cur.execute(
-    "SELECT direct_url, price, size_list, upsell_product_desc FROM catalog_for_website WHERE color_or_style = %s",
-    (option,),
-)
-df2 = my_cur.fetchone()
+    # We'll build the image caption now since we can
+    product_caption = f'Our warm, comfortable, {option} sweatsuit!'
 
-# Check if df2 is not None before using its values
-if df2:
-    streamlit.image(df2[0], width=400, caption=product_caption)
-    streamlit.write('Price: ', df2[1])
-    streamlit.write('Sizes Available: ', df2[2])
-    streamlit.write(df2[3])
+    # Use the selected option to retrieve all the info from the database using a parameterized query
+    my_cur.execute(
+        "SELECT direct_url, price, size_list, upsell_product_desc FROM catalog_for_website WHERE color_or_style = %s",
+        (option,),
+    )
+    df2 = my_cur.fetchone()
 
-# Close the Snowflake cursor and connection
-my_cur.close()
-my_cnx.close()
+    # Check if df2 is not None before using its values
+    if df2:
+        streamlit.image(df2[0], width=400, caption=product_caption)
+        streamlit.write('Price: ', df2[1])
+        streamlit.write('Sizes Available: ', df2[2])
+        streamlit.write(df2[3])
+
+    # Close the Snowflake cursor and connection
+    my_cur.close()
+    my_cnx.close()
+
+except AttributeError:
+    streamlit.warning("This script is not running in a Streamlit environment.")
+
+except snowflake.connector.errors.ProgrammingError as e:
+    streamlit.error(f"Snowflake ProgrammingError: {e}")
+
+except snowflake.connector.errors.DatabaseError as e:
+    streamlit.error(f"Snowflake DatabaseError: {e}")
