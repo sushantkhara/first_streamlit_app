00353702619476f8771215f282664fc9338d52fd
Sushant Khara <sushantkumarkhara@gmail.com>
2023-11-11 12:37:43 +0530

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,22 +1,42 @@
 import streamlit
 import snowflake.connector
 
-# Connect to Snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+# Check if running in Streamlit environment
+if streamlit._is_running_with_streamlit:
 
-# Execute Snowflake query
-my_cur.execute(
-    "SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()"
-)
+    # Load Snowflake secrets
+    snowflake_secrets = streamlit.secrets["snowflake"]
 
-# Fetch the result
-my_data_row = my_cur.fetchone()
+    # Establish Snowflake connection
+    try:
+        my_cnx = snowflake.connector.connect(
+            user=snowflake_secrets["user"],
+            password=snowflake_secrets["password"],
+            account=snowflake_secrets["account"],
+            warehouse=snowflake_secrets["warehouse"],
+            database=snowflake_secrets["database"],
+            schema=snowflake_secrets["schema"],
+        )
 
-# Display result
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+        my_cur = my_cnx.cursor()
 
-# Close the cursor and connection
-my_cur.close()
-my_cnx.close()
+        # Execute Snowflake query
+        my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+        my_data_row = my_cur.fetchone()
+
+        # Display result
+        streamlit.text("Hello from Snowflake:")
+        streamlit.text(my_data_row)
+
+    except snowflake.connector.errors.ProgrammingError as e:
+        streamlit.error(f"Snowflake error: {e}")
+
+    finally:
+        # Close the cursor and connection in a finally block
+        if my_cur:
+            my_cur.close()
+        if my_cnx:
+            my_cnx.close()
+
+else:
+    streamlit.warning("This script is not running in a Streamlit environment.")
