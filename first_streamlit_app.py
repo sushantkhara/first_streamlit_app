4a5c411b380874f367dfc39081a142a0f5f68a23
Sushant Khara <sushantkumarkhara@gmail.com>
2023-11-11 12:36:06 +0530

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -1,36 +1,22 @@
 import streamlit
 import snowflake.connector
 
-# Load Snowflake secrets
-snowflake_secrets = streamlit.secrets["snowflake"]
+# Connect to Snowflake
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
 
-# Establish Snowflake connection
-try:
-    my_cnx = snowflake.connector.connect(
-        user=snowflake_secrets["user"],
-        password=snowflake_secrets["password"],
-        account=snowflake_secrets["account"],
-        warehouse=snowflake_secrets["warehouse"],
-        database=snowflake_secrets["database"],
-        schema=snowflake_secrets["schema"],
-    )
+# Execute Snowflake query
+my_cur.execute(
+    "SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()"
+)
 
-    my_cur = my_cnx.cursor()
+# Fetch the result
+my_data_row = my_cur.fetchone()
 
-    # Execute Snowflake query
-    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-    my_data_row = my_cur.fetchone()
+# Display result
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
-    # Display result
-    streamlit.text("Hello from Snowflake:")
-    streamlit.text(my_data_row)
-
-except snowflake.connector.errors.ProgrammingError as e:
-    streamlit.error(f"Snowflake error: {e}")
-
-# Close the cursor and connection in a finally block
-finally:
-    if my_cur:
-        my_cur.close()
-    if my_cnx:
-        my_cnx.close()
+# Close the cursor and connection
+my_cur.close()
+my_cnx.close()
